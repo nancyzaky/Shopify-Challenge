2d7eb545495ede88f2e894065f57923b747d2003
Nancy Zaky <nancyzki@yahoo.com>
2022-01-14 12:12:18 -0700

    react loading package

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ReactLoading from "react-loading";
 
 const Loading = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "4rem",
+      }}
+    >
+      <ReactLoading type="balls" color="orange" height={167} width={275} />;
+    </div>
+  );
 };
 
 export default Loading;
