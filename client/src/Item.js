47ba9c6c8a059836f5608dedb51f534de811a8ef
Nancy Zaky <nancyzki@yahoo.com>
2022-01-14 12:11:46 -0700

    descriptio/ title/date

--- client/src/Item.js
+++ client/src/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Item = ({ item }) => {
-  console.log(item.hdurl);
+  const alter =
+    "https://apod.nasa.gov/apod/image/1501/SevenStrongSky_sergei_960.jpg";
+  const [clicked, setClicked] = useState(false);
   return (
-    <div>
-      <img src={item.hdurl} alt="pic" className="image" />
-    </div>
+    <li className="img-cont">
+      <h4>{item.title}</h4>
+      <img src={item.hdurl || alter} alt="pic" className="image" />
+      <h4>{item.date}</h4>
+      <button
+        className="btn"
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      >
+        {clicked ? `Unlike` : `Like`}
+      </button>
+    </li>
   );
 };
 
